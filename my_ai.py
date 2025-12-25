686cf1bb1de845995462101199c2905d7c2fc1f2
CGMA006 <pranjaljoshi095@gmail.com>
2025-12-25 19:25:52 +0545

    Learning Vectorization

--- my_ai.py
+++ my_ai.py
@@ -1,27 +1,44 @@
 import nltk
-import matplotlib.pyplot as plt
+import string
+from nltk.stem import WordNetLemmatizer
 
-nltk.download('punkt')
-nltk.download('punkt_tab')
+#nltk.download('wordnet')
+#nltk.download('punkt_tab')
 
-text = "hattile padyo pwa pwa pwa. phush phush"
+sentences = ["Hattile padyo pwa.",
+            "Musale padyo phush phush."]
+lemmatizer = WordNetLemmatizer()
 
-from nltk.tokenize import word_tokenize,sent_tokenize
-from nltk.probability import FreqDist
-# print(word_tokenize(text))
-
-# print (sent_tokenize(text))
-
-fd = FreqDist(text)
-
-print (fd.most_common())
-
-fd.plot()
-
-plt.show()
+def normalize(sentence):
+    tokens = nltk.word_tokenize(sentence.lower())
+    lemmas = [lemmatizer.lemmatize(t) for t in tokens]
+    clean = [w for w in lemmas if w not in string.punctuation]
+    return clean
 
 
+normal = [normalize(s) for s in sentences]
 
 
+all_words = []
+for s in normal:
+    all_words.extend(s)
 
+vocab = sorted(set(all_words))
+print("Vocabulary:", vocab)
 
+def bag_of_words(sentence,vocab):
+    sentence_words = normalize(sentence)
+    
+    # Create vector: 1 if word exists, 0 otherwise
+    vector = []
+    for word in vocab:
+     if word in sentence_words:
+        vector.append(1)
+     else:
+        vector.append(0)
+    
+    return vector
+
+sentences = ["Hattile padyo pwa.", "Musale padyo phush phush."]
+vectors = [bag_of_words(s, vocab) for s in sentences]
+print(vectors)
